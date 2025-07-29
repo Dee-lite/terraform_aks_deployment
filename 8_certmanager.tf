93e0993b5ba943059b054db5c72b384cfff5989f
Dee-lite <damilola.onifade45@gmail.com>
2025-07-29 21:41:54 +0100

    redploying infra

--- 8_certmanager.tf
+++ 8_certmanager.tf
@@ -1,14 +1,15 @@
 resource "helm_release" "cert_manager" {
-  name = "cert-manager"
-
+  name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
   namespace        = "cert-manager"
   create_namespace = true
   version          = "v1.18.2"
 
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
 }
